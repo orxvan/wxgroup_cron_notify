307593e5c977806605800f9d21d1002c96f8afb8
yangshuo <yangshuo@aisino.com>
2021-09-22 14:17:01 +0800

    update command

--- wxgroup_cron_notify.py
+++ wxgroup_cron_notify.py
@@ -2,14 +2,17 @@ from chinese_calendar import is_workday
 import datetime
 import requests
 import os
+import typer
 
 # use the service https://github.com/tans/push-bot
 
-
 TOKEN = os.environ['TOKEN']
 
+app = typer.Typer()
 
-def send_wx_group_msg(token, msg):
+
+@app.command()
+def send_wx_group_msg(msg: str, token=TOKEN):
     if (is_workday(datetime.date.today())):
         url = "https://push.bot.qw360.cn/room/"+token+"?msg="+msg
         r = requests.get(url)
@@ -17,4 +20,5 @@ def send_wx_group_msg(token, msg):
 
 
 if __name__ == '__main__':
-    send_wx_group_msg(TOKEN, "hello world")
+    # send_wx_group_msg("hello world", TOKEN)
+    app()
